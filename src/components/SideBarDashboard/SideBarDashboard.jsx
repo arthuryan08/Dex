55dc7cce16382eada35ddb32c0626ff4182771ef
Arthur Yan <arthuryan08@gmail.com>
2021-12-19 17:10:03 -0300

    Dashboard done

--- src/components/SideBarDashboard/SideBarDashboard.jsx
+++ src/components/SideBarDashboard/SideBarDashboard.jsx
@@ -1,49 +1,46 @@
-import React from 'react'
+import React from "react";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import './sideBarDashboard.css'
+import "./sideBarDashboard.css";
 
-import logo from '../../assets/logo.svg'
+import logo from "../../assets/logo.svg";
 
-import sidebar_items from '../../assets/JsonData/sidebar_routes.json'
+import sidebar_items from "../../assets/JsonData/sidebar_routes.json";
 
-const SidebarItem = props => {
-  const active = props.active ? 'active' :''
+const SidebarItem = (props) => {
+  const active = props.active ? "active" : "";
 
   return (
     <div className="sidebar__item">
       <div className={`sidebar__item-inner ${active}`}>
         <i className={props.icon}></i>
-        <span>
-          {props.title}
-        </span>
+        <span>{props.title}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const SideBarDashboard = props => {
-
-  const activeItem = sidebar_items.findIndex(item => item.route === props.location.pathname)
+const SideBarDashboard = (props) => {
+  const activeItem = sidebar_items.findIndex(
+    (item) => item.route === props.location.pathname
+  );
   return (
     <div className="sidebar">
       <div className="sidebar__logo">
-        <img src={logo} alt="company logo"/>
+        <img src={logo} alt="company logo" />
       </div>
-      {
-        sidebar_items.map((item, index) => (
-          <Link to={item.route} key={index}>
-            <SidebarItem 
-              title={item.display_name}
-              icon={item.icon}
-              active={index === activeItem}
-            />
-          </Link>
-        ))
-      }
+      {sidebar_items.map((item, index) => (
+        <Link to={item.route} key={index}>
+          <SidebarItem
+            title={item.display_name}
+            icon={item.icon}
+            active={index === activeItem}
+          />
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default SideBarDashboard
+export default SideBarDashboard;
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,81 +1,81 @@
-import React from 'react'
-import './topnav.css'
+import React from "react";
 
-import { Link } from 'react-router-dom'
+import "./topnav.css";
 
-import Dropdown from '../Dropdown/Dropdown'
+import { Link } from "react-router-dom";
 
-import notifications from '../../assets/JsonData/notification.json'
+import Dropdown from "../Dropdown/Dropdown";
 
-import user_image from '../../assets/arthur.jpg'
+import ThemeMenu from "../ThemeMenu/ThemeMenu";
 
-import user_menu from '../../assets/JsonData/user-menus.json'
+import notifications from "../../assets/JsonData/notification.json";
+
+import user_image from "../../assets/arthur.jpg";
+
+import user_menu from "../../assets/JsonData/user-menus";
 
 const curr_user = {
-  display_name: 'Arthur Yan',
-  image: user_image
-}
+  display_name: "Tuat Tran",
+  image: user_image,
+};
 
 const renderNotificationItem = (item, index) => (
   <div className="notification-item" key={index}>
     <i className={item.icon}></i>
     <span>{item.content}</span>
   </div>
-)
+);
 
 const renderUserToggle = (user) => (
   <div className="topnav__right-user">
     <div className="topnav__right-user__image">
       <img src={user.image} alt="" />
     </div>
-    <div className="topnav__right-user__name">
-      {user.display_name}
-    </div>
+    <div className="topnav__right-user__name">{user.display_name}</div>
   </div>
-)
+);
 
-const renderUserMenu =(item, index) => (
-  <Link to='/' key={index}>
-      <div className="notification-item">
-          <i className={item.icon}></i>
-          <span>{item.content}</span>
-      </div>
+const renderUserMenu = (item, index) => (
+  <Link to="/" key={index}>
+    <div className="notification-item">
+      <i className={item.icon}></i>
+      <span>{item.content}</span>
+    </div>
   </Link>
-)
+);
 
-const TopNav = () => {
+const Topnav = () => {
   return (
-    <div className='topnav'>
+    <div className="topnav">
       <div className="topnav__search">
         <input type="text" placeholder="Search here..." />
-        <i className='bx bx-search'></i>
+        <i className="bx bx-search"></i>
       </div>
       <div className="topnav__right">
         <div className="topnav__right-item">
-          <Dropdown 
+          {/* dropdown here */}
+          <Dropdown
             customToggle={() => renderUserToggle(curr_user)}
             contentData={user_menu}
-            renderItems={(item,index) => renderUserMenu(item, index)}
+            renderItems={(item, index) => renderUserMenu(item, index)}
           />
-          {/* dropdown here */}
         </div>
         <div className="topnav__right-item">
           <Dropdown
-            icon='bx bx-bell'
-            badge='12'
+            icon="bx bx-bell"
+            badge="12"
             contentData={notifications}
             renderItems={(item, index) => renderNotificationItem(item, index)}
-            renderFooter={() => <Link to='/'>View All</Link>}
+            renderFooter={() => <Link to="/">View All</Link>}
           />
           {/* dropdown here */}
         </div>
         <div className="topnav__right-item">
-          <Dropdown />
-          {/* theme setting */}
+          <ThemeMenu />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopNav;
+export default Topnav;
