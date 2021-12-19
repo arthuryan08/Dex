6e8baa8fb801d0236687348f01fa8a91be038978
Arthur Yan <arthuryan08@gmail.com>
2021-12-19 14:39:24 -0300

    Dashboard SideBar done

--- src/components/SideBarDashboard/SideBarDashboard.jsx
+++ src/components/SideBarDashboard/SideBarDashboard.jsx
@@ -12,7 +12,7 @@ const SidebarItem = props => {
   const active = props.active ? 'active' :''
 
   return (
-    <div className="sidebar_item">
+    <div className="sidebar__item">
       <div className={`sidebar__item-inner ${active}`}>
         <i className={props.icon}></i>
         <span>
