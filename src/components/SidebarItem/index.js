984afb9726fa7fc686be554500a429ae7dba7844
Arthur Yan <arthuryan08@gmail.com>
2021-12-20 14:22:04 -0300

    Update index.js

--- src/components/SidebarItem/index.js
+++ src/components/SidebarItem/index.js
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import * as C from "./styles";
 import { GiSuitcase } from "react-icons/gi";
 import { IoIosBrush } from "react-icons/io";
@@ -18,7 +18,7 @@ export const SidebarItem = ({ title, description, icon, path, active }) => {
         <C.IconArea active={active} step={history.location.pathname}>
           {icon === "profile" && <GiSuitcase fill="white" size="45px" />}
           {icon === "book" && <IoIosBrush fill="white" size="35px" />}
-          {icon === "mail" && <CgScreen color="white" size='35px'/>}
+          {icon === "mail" && <CgScreen color="white" size="35px" />}
         </C.IconArea>
         <C.Point active={active} step={history.location.pathname}></C.Point>
       </Link>
