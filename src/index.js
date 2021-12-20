ec7fa6892ac7afc1b9a2add8da5572aff50534d9
Arthur Yan <arthuryan08@gmail.com>
2021-12-20 03:48:19 -0300

    update

--- src/index.js
+++ src/index.js
@@ -3,29 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
 
-import { createStore } from 'redux'
-
-import { Provider } from 'react-redux'
-
-import rootReducer from './redux/reducers'
-
-import "./assets/boxicons-2.1.1/css/boxicons.min.css";
-import "./assets/cssdashboard/grid.css";
-import "./assets/cssdashboard/index.css";
-import "./assets/cssdashboard/theme.css";
-
-import Layout from "./components/LayoutDashboard/Layout";
-
-const store = createStore(
-  rootReducer
-)
-
-
 ReactDOM.render(
-  <Provider store={store}>
     <React.StrictMode>
-      <Layout />
-    </React.StrictMode>
-  </Provider>,
+      <App />
+    </React.StrictMode>,
   document.getElementById("root")
 );
